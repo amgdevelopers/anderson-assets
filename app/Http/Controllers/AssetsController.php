e3ea65df52eb739791c85a0ff9d95ebaa828a778
Orlando Villasenor <Orlando@m-171.local>
2019-10-25 13:50:27 -0500

    updated upload function

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Asset;
 use App\Client;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Chumper\Zipper\Facades\Zipper;
 use Illuminate\Support\Facades\Storage;
@@ -41,7 +42,47 @@ class AssetsController extends Controller
             [ 'uri' => $request->job . '-' . $request->size, 'client_dir' => $client->directory ]
         );
 
-        Zipper::make( $request->file('asset') )->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ) );
+        $zip = new \ZipArchive;
+        $path = $request->file('asset');
+        if ( $zip->open($path) === true ) 
+        {
+            for( $i = 0; $i < $zip->numFiles; $i++ ) 
+            {
+                $filename = $zip->getNameIndex($i);
+                $fileinfo = pathinfo($filename);
+
+                if( stripos($fileinfo['dirname'], '__MACOSX') !== false || !isset($fileinfo['extension']) ) {
+                    $zip->deleteIndex($i);
+                    continue;
+                }
+                
+                $files[] = $filename;
+            }
+
+            if( in_array('index.html', $files) ) {
+                $zip->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ), $files);
+            } else {
+                // this does not work yet
+                foreach ( $files as $file ) 
+                {
+                    $slice = Str::before($file, 'index.html');
+                    if( $slice !== $file ) {
+                        break;
+                    }
+                }
+
+                $filtered_files = array_map(function($file) use ($slice) {
+                    return str_replace($slice, '', $file);
+                }, $files);
+
+                // $zip->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ), $filtered_files);
+                dd($filtered_files);
+            }
+            
+            $zip->close();                  
+        }
+
+        // Zipper::make( $request->file('asset') )->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ) );
 
         return redirect()->action('HomeController@index');
     }
