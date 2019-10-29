669ed3b378c660af245bdf4f8939ff48c9265f6b
Orlando Villasenor <Orlando@m-171.local>
2019-10-29 18:42:05 -0500

    Refactor of Asset model and AssetsController class

--- app/Asset.php
+++ app/Asset.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
@@ -11,5 +13,49 @@ class Asset extends Model
     public function client()
     {
     	return $this->belongsTo('App\Client', 'directory', 'client_dir');
+    }
+
+    /**
+     * Prepare the uploaded zip and extract the necessary files to the appropriate directory.
+     *
+     * @param  \Illuminate\Http\UploadedFile $file
+     * @return void
+     */
+    public function upload( $file )
+    {
+
+    	$zip = new \ZipArchive;
+    	if ( $zip->open( $file ) === true ) 
+    	{
+    	    for( $i = 0; $i < $zip->numFiles; $i++ ) 
+    	    {
+    	        $filename = $zip->getNameIndex($i);
+    	        $fileinfo = pathinfo($filename);
+
+    	        if( stripos($fileinfo['dirname'], '__MACOSX') !== false || !isset($fileinfo['extension']) ) {
+    	            $zip->deleteIndex($i);
+    	            continue;
+    	        }
+    	        
+    	        $paths[] = $filename;
+    	    }
+
+    	    if( in_array('index.html', $paths) ) {
+    	        $zip->extractTo( storage_path('app/public/' . $this->client_dir . '/' . $this->uri ), $paths);
+    	    } else {
+    	        $index_html = Arr::first($paths, function ($value, $key) {
+    	            return Str::endsWith($value, '/index.html');
+    	        });
+    	        $slice = Str::before($index_html, 'index.html');
+
+    	        foreach ( $paths as $path ) 
+    	        {
+    	            $new_name = str_replace($slice, '', $path);
+    	            $zip->renameName($path, $new_name);
+    	            $zip->extractTo( storage_path('app/public/' . $this->client_dir . '/' . $this->uri ), $new_name);
+    	        }
+    	    }
+    	    $zip->close();
+    	}
     }
 }
--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Asset;
 use App\Client;
 use Illuminate\Support\Str;
-use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -41,40 +40,7 @@ class AssetsController extends Controller
             [ 'uri' => $request->job . '-' . $request->size, 'client_dir' => $client->directory ]
         );
 
-        $zip = new \ZipArchive;
-        $path = $request->file('asset');
-        if ( $zip->open($path) === true ) 
-        {
-            for( $i = 0; $i < $zip->numFiles; $i++ ) 
-            {
-                $filename = $zip->getNameIndex($i);
-                $fileinfo = pathinfo($filename);
-
-                if( stripos($fileinfo['dirname'], '__MACOSX') !== false || !isset($fileinfo['extension']) ) {
-                    $zip->deleteIndex($i);
-                    continue;
-                }
-                
-                $files[] = $filename;
-            }
-
-            if( in_array('index.html', $files) ) {
-                $zip->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ), $files);
-            } else {
-                $index_html = Arr::first($files, function ($value, $key) {
-                    return Str::endsWith($value, '/index.html');
-                });
-                $slice = Str::before($index_html, 'index.html');
-
-                foreach ( $files as $file ) 
-                {
-                    $new_name = str_replace($slice, '', $file);
-                    $zip->renameName($file, $new_name);
-                    $zip->extractTo( storage_path('app/public/' . $client->directory . '/' . $asset->uri ), $new_name);
-                }
-            }
-            $zip->close();                  
-        }
+        $asset->upload( $request->file('asset') );
 
         return redirect()->action('HomeController@index');
     }
